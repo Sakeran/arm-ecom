f5189b1f7360f6569fb6d55bb175e73edf7f6565
Alexander Morse <sakeran@gmail.com>
2019-06-28 09:01:03 -0400

    add menu button toggle

--- src/components/header.js
+++ src/components/header.js
@@ -17,6 +17,13 @@ const ToggleButton = styled.button`
   }
   top: 0.5rem;
   right: 0.5rem;
+  background: none;
+  border: 0;
+
+  svg {
+    width: 3rem;
+    height: auto;
+  }
 `
 
 const Menu = styled.div`
@@ -66,7 +73,33 @@ class Header extends React.Component {
           <h1>{this.props.siteTitle}</h1>
         </ColorBand>
         <nav>
-          <ToggleButton onClick={this.toggleState}>MENU</ToggleButton>
+          <ToggleButton
+            onClick={this.toggleState}
+            aria-expanded={this.state.menuToggled}
+            aria-label="Open Menu"
+          >
+            {this.state.menuToggled ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                <path d="M0 0h24v24H0z" fill="none" />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+              </svg>
+            )}
+          </ToggleButton>
           <Menu aria-expanded={this.state.menuToggled}>
             <ul>
               <li>
