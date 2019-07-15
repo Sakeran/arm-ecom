5e2ceb034aa49135e782657e2492199b726b3854
Alexander Morse <sakeran@gmail.com>
2019-07-15 09:48:55 -0400

    add cart action creators

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,6 +1,9 @@
 export const LOGIN = "LOGIN"
 export const LOGOUT = "LOGOUT"
 export const CHANGE_SORT = "CHANGE_SORT"
+export const ADD_ITEM = "ADD_ITEM"
+export const REMOVE_ITEM = "REMOVE_ITEM"
+export const CLEAR_CART = "CLEAR_CART"
 
 export const login = username => ({
   type: LOGIN,
@@ -14,4 +17,14 @@ export const logout = () => ({
 export const changeSort = sort => ({
   type: CHANGE_SORT,
   preferredSort: sort,
+})
+
+export const addItem = item => ({
+  type: ADD_ITEM,
+  item
+})
+
+export const removeItem = itemKey => ({
+  type: REMOVE_ITEM,
+  itemKey
 })
